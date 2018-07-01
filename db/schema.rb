98a684c9f50821b10cf7d8e582bcc4b70a165726
Benj <benj@rocksbox.com>
2018-07-01 12:27:24 -0700

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701191808) do
+ActiveRecord::Schema.define(version: 20180701192625) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
